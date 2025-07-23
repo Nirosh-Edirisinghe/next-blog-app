410616e5b5b048ba474c17f3494205e93aff4f7c
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2025-07-24 01:33:59 +0530

    change layout of the admin page

--- app/admins/layout.jsx
+++ app/admins/layout.jsx
@@ -1,12 +1,21 @@
+import { assets } from "@/assets/assets";
 import SlideBar from "@/Components/AdminComponents/SlideBar";
+import Image from "next/image";
 
-export default function Layout({children}){
-  return(
+export default function Layout({ children }) {
+  return (
     <>
       <div className="flex">
-        <SlideBar/>
+        <SlideBar />
+        <div className="flex flex-col w-full">
+          <div className="flex items-center justify-between w-full py-3 max-h-[600px] px-12 border-b border-black">
+            <h3 className="font-medium">Admin panel</h3>
+            <Image src={assets.profile_icon} width={40} alt="profile" />
+          </div>
+          {children}
+        </div>
       </div>
-      {children}
+
     </>
   )
 }
