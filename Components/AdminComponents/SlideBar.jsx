e1cf8a7820856f18cd86fedbb9eb0880be6be36a
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2025-07-24 01:34:33 +0530

    create add product page

--- Components/AdminComponents/SlideBar.jsx
+++ Components/AdminComponents/SlideBar.jsx
@@ -10,7 +10,7 @@ const SlideBar = () => {
         <Image src={assets.logo} width={120} alt='logo' />
       </div>
 
-      <div className="w-28 sm:w-80 h-[100vh] relative py-12 border border-black">
+      <div className="w-28 sm:w-80 h-[100%] relative py-12 border border-black">
         <div className='w-[50%] sm:w-[80%] absolute right-0'>
           <Link href='/admins/addProduct' className="flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-5px_5px_0px_#000000]">
             <Image src={assets.add_icon} alt='add' /><p>Add blogs</p>
